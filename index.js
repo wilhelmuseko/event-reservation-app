4e41ddbea8293ec1ff3f5df95b18841bd685a040
Herriyandi <herriyandi.herriyandi@docdoc.com>
2021-10-17 14:57:24 +0700

    create db connection and create sample route

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
+app.use(express.json());
+
+app.get('/event/get_info', async (req, res) => {
+  try {
+    res.json({
+      name: 'asdfasdf',
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
 });
--- resources/db/script.sql
+++ resources/db/script.sql
@@ -1,57 +1,55 @@
-create schema if not exists event_reservation;
+drop database if exists event_reservation_database;
+create database event_reservation_database;
+\c event_reservation_database;
 
-create table event_reservation."event"(
+create table "event"(
 	id uuid primary key,
 	"name" text not null unique,
 	start_date timestamp not null,
 	end_date timestamp not null
 );
 
-create table event_reservation."location"(
+create table "location"(
 	id uuid primary key,
 	"name" text not null,
 	address text not null
 );
 
-create table event_reservation.event_location(
+create table event_location(
 	id uuid primary key,
-	event_id uuid not null unique references event_reservation."event",
-	location_id uuid not null references event_reservation."location"
+	event_id uuid not null unique references "event",
+	location_id uuid not null references "location"
 );
 
-create table event_reservation.ticket_type(
+create table ticket_type(
 	id uuid primary key,
 	"type" text not null unique,
 	quota int4 not null,
 	price int8 not null
 );
 
-create table event_reservation.event_ticket_type(
+create table event_ticket_type(
 	id uuid primary key,
-	event_id uuid unique references event_reservation."event",
-	ticket_type_id uuid unique references event_reservation."ticket_type"
+	event_id uuid unique references "event",
+	ticket_type_id uuid unique references "ticket_type"
 );
 
-create table event_reservation.customer(
+create table customer(
 	id uuid primary key,
 	"name" text not null,
 	date_of_birth date not null,
 	phone_number text not null unique
 );
 
-create table event_reservation.ticket_purchase(
+create table ticket_purchase(
 	id uuid primary key,
-	customer_id uuid not null references event_reservation.customer,
+	customer_id uuid not null references customer,
 	date_of_purchase timestamp
 );
 
-create table event_reservation.ticket_purchase_detail(
+create table ticket_purchase_detail(
 	id uuid primary key,
-	ticket_purchase_id uuid not null references event_reservation.ticket_purchase,
-	event_ticket_type_id uuid not null references event_reservation.event_ticket_type,
+	ticket_purchase_id uuid not null references ticket_purchase,
+	event_ticket_type_id uuid not null references event_ticket_type,
 	quantity int4
 );
-
-
-
-
